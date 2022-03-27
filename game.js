94990ca373d5309397f4c301fcc52dc648d5ddb9
Aashish Pokharel <pokharellaashish454@gmail.com>
2022-03-27 22:39:33 +0545

    [add] initial working minimax added

--- game.js
+++ game.js
@@ -31,15 +31,17 @@ function insertSymbol(element, symbol){
     element.innerHTML = symbol;
     if(symbol == enemySymbol){
         document.getElementById(element.id).classList.add('player-two');
-        document.getElementById(element.id).classList.add('cannotuse');
     }
+    document.getElementById(element.id).classList.add('cannotuse');
     checkWinConditions(element);
     turn++;
+    console.log(cpuEnabled);
     if(win || turn>8){
         document.getElementById('restart').classList.add('btn-green');
-
+        // document.getElementById(element.id).classList.add('cannotuse');
     }else if(cpuEnabled && turn%2 !== 0){
-        cpuTurn();
+        // cpuTurn();
+        bestMove();
     }
 
 }
@@ -60,6 +62,7 @@ function checkWinConditions(element){
     row = element.id[4];
     column= element.id[5];
     win = true;
+    // Horizontal Win condition
     for(var i = 0;i<3;i++){
         if(document.getElementById('cell'+row+i).textContent !== element.innerHTML){
             win= false;
@@ -69,7 +72,50 @@ function checkWinConditions(element){
         for(var i = 0;i<3;i++){
             document.getElementById('cell'+row+i).classList.add('wincell')
         }
+        return;
     }
+    // Vertical
+    win=true;
+    for(var i = 0;i<3;i++){
+        if(document.getElementById('cell'+i+column).textContent !== element.innerHTML){
+            win= false;
+        }
+    }
+    if(win){
+        for(var i = 0;i<3;i++){
+            document.getElementById('cell'+i+column).classList.add('wincell');
+        }
+        return;
+    }
+    //main diagonal
+    win=true;
+    for(var i = 0;i<3;i++){
+        if(document.getElementById('cell'+i+i).textContent !== element.innerHTML){
+            win= false;
+        }
+    }
+    if(win){
+        for(var i = 0;i<3;i++){
+            document.getElementById('cell'+i+i).classList.add('wincell');
+        }
+        return;
+    }
+    // Secondary Diagonal
+    win= false;
+    if(document.getElementById('cell02').textContent == element.innerHTML){
+        if(document.getElementById('cell11').textContent == element.innerHTML){
+            if(document.getElementById('cell20').textContent == element.innerHTML){
+                win = true;
+                document.getElementById('cell02').classList.add('wincell');
+                document.getElementById('cell11').classList.add('wincell');
+                document.getElementById('cell20').classList.add('wincell');
+            }
+        }
+
+    }
+
+
+
 }
 
 function cpuTurn(){
@@ -81,6 +127,30 @@ function cpuTurn(){
         if(document.getElementById('cell'+row+column).innerHTML == ''){
             ok = true;
         }
+        document.getElementById('cell'+row+column).click();
     }
-    console.log('unsuccessful');
+}
+
+function bestMove(){
+    
+    var row;
+    var column;
+    let bestScore = -Infinity;
+    let move ;
+    for(row = 0;row<3;row++){
+        for(column= 0;column<3;column++){
+            if(document.getElementById('cell'+row+column).innerHTML == ''){
+                let score = minimax(0, true);
+                if(score > bestScore){
+                    bestScore = score;
+                    move = [row, column]
+                }
+            }  
+        }
+    }
+        document.getElementById('cell'+move[0]+move[1]).click();
+    }
+function minimax(depth, isMaximizing){
+    return 1;
+    
 }
